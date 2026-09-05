c01a89bef39493c2c2c34ed9f0aab6b68ee87f65
ceoanhh <linh7acute@gmail.com>
2026-09-05 14:13:19 +0700

    core: initialize Spring Boot project

---
+++ src/main/java/com/vaxflow/VaxflowApplication.java
@@ -0,0 +1,13 @@
+package com.vaxflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaxflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaxflowApplication.class, args);
+	}
+
+}
