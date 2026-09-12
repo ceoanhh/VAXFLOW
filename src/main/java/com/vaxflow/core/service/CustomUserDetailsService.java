e5fb5426f4b8d1476e4e3aeeb1d5f22b3c13c25d
ceoanhh <linh7acute@gmail.com>
2026-09-12 17:34:22 +0700

    core: add CustomUserDetailsService

---
+++ src/main/java/com/vaxflow/core/service/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.vaxflow.core.service;
+
+import com.vaxflow.core.entity.Admin;
+import com.vaxflow.core.repository.AdminRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final AdminRepository adminRepository;
+
+    public CustomUserDetailsService(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        Admin admin = adminRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "Không tìm thấy tài khoản đăng nhập"
+                        )
+                );
+
+        return User.withUsername(admin.getUsername())
+                .password(admin.getPassword())
+                .roles("ADMIN")
+                .build();
+    }
+}
